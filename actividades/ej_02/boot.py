23c3732ab9bc42e2eac644129d361a8d3947558d
DonattiAugusto <83584451+DonattiAugusto@users.noreply.github.com>
2024-10-22 15:22:50 -0300

    Update boot.py

--- actividades/ej_02/boot.py
+++ actividades/ej_02/boot.py
@@ -1,1 +1,14 @@
-# Configuracion inicial
+def establecer_conexion():
+    import network
+    from time import sleep
+    interfaz_wifi = network.WLAN(network.STA_IF)
+    if not interfaz_wifi.isconnected():
+        print("Conectando a la red...")
+        interfaz_wifi.active(True)
+          interfaz_wifi.connect("Cooperadora Alumnos", "")
+          while not interfaz_wifi.isconnected():
+            print(".", end="")
+            sleep(0.25)
+    print("Configuración de la red:", interfaz_wifi.ifconfig())
+
+establecer_conexion()
